a9c1b7c542b6e7b658e9f7e2b18dab9965748d86
JosefaHerrera <josefa@linuxero.cl>
2017-07-21 19:14:30 -0400

    validacion nombre lista

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+//index.html validar form.
+//Expresión para validar un correo electrónico expresiones regulares
+var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+
 function validateForm(){
 	/* Escribe tú código aquí */
+	    // al hacer click en el btn-enviar, empezará a validar el formulario
+    $("#btn-enviar").click(function(){
+    var nombre = $("#name").val();
+    var apellido = $("#lastname").val();
+    var correo = $("#input-email").val();
+    var pwd = $("#input-password").val();
+    //fadeIn(); hace un tipo de animacion de opacidad 0% a 100%
+    if(nombre == ""){
+        $("#mensaje1").fadeIn("slow");
+        ////al tener el valor false , es que no esta validando el contenido como lo hace true.
+            return false;
+        }else{
+        //fadeOut(); hace un tipo de animacion de opacidad 100% a 0%
+              $("#mensaje1").fadeOut();
+             }if(apellido == "" ){
+                    $("#mensaje2").fadeIn("slow");
+                    return false;
+                }
+                
+    });
 }
+
+
+
