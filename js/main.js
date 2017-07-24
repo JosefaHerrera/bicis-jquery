6b0bd628b0d6f5588f8cba8d854c6a305f27acf5
JosefaHerrera <josefa@linuxero.cl>
2017-07-23 21:57:17 -0400

    validaciones al 100%

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 
 
 function validateForm(){
-	/* Escribe tú código aquí */
-	    // al hacer click en el btn-enviar, empezará a validar el formulario
+    /* Escribe tú código aquí */
+        // al hacer click en el btn-enviar, empezará a validar el formulario
     $("#btn-enviar").click(function(){
     var nombre = $("#name").val();
     var apellido = $("#lastname").val();
     var correo = $("#input-email").val();
     var pwd = $("#input-password").val();
+    var largoPass = pwd.length;
     //fadeIn(); hace un tipo de animacion de opacidad 0% a 100%
     if(nombre == ""){
         $("#mensaje1").fadeIn("slow");
@@ -26,24 +27,22 @@ function validateForm(){
                 else{
                     $("#mensaje2").fadeOut();
                     if(correo == "" || !expr.test(correo)){
-        				$("#mensaje3").fadeIn("slow");
-        				////al tener el valor false , es que no esta validando el contenido como lo hace true.
-            			return false;
-        			 }else{
-        			 	$("#mensaje3").fadeOut();
-        			 	if( pwd == ""){
-        			 		$("#mensaje4").fadeIn("slow");
-        			 	}else if( pwd == 123456 || pwd == 098765){
-        			 		$("#mensaje4").fadeOut();
-        			 		$("#mensaje5").fadeIn("slow");
-        			 	}else{
-        			 		$("#mensaje5").fadeOut();
-        			 		$("#mensaje4").fadeOut();
-        			 	}
-        			 }
+                        $("#mensaje3").fadeIn("slow");
+                        ////al tener el valor false , es que no esta validando el contenido como lo hace true.
+                        return false;
+                     }else{
+                        $("#mensaje3").fadeOut();
+                        if( pwd == ""){
+                            $("#mensaje4").fadeIn("slow");
+                        }else if(largoPass < 6 || pwd == 123456 || pwd == 098765 || pwd == 'password'){
+                            $("#mensaje4").fadeOut();
+                            $("#mensaje5").fadeIn("slow");
+                        }else{
+                            $("#mensaje5").fadeOut();
+                            $("#mensaje4").fadeOut();
+                        }
+                     }
                 }
     });
 }
-
-
 
